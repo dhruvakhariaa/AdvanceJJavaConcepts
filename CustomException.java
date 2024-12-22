c14bb01816059a94348576085bfb9e456012c890
dhruvakhariaa <vakhariadhruv526@gmail.com>
2024-12-22 21:07:33 +0530

    Adjusted brackets

--- CustomException.java
+++ CustomException.java
@@ -12,27 +12,22 @@ class DhruvException extends Exception{
 			int j=0;
 	 
 			
-			try
-			{
-				j=18/i;
-				if(j==0)
-	//    			throw new Exception("I don't want to do print zero");
-					throw new DhruvException("I dont want to print zero");
-    	}
-
-    	catch(ArithmeticException e)
+		try{
+			j=18/i;
+			if(j==0)
+	//    		throw new Exception("I don't want to do print zero");
+				throw new DhruvException("I dont want to print zero");
+    	}catch(ArithmeticException e)
     	{
     		j=20/i;
     		System.out.println("that is default output"+e);
 
 //    		System.out.println("Cannot divide by zero");
-    	}
-    	
-    	catch(Exception e)
-    	{
+    	}catch(Exception e)
+		{
   
-    		System.out.println("Something went wrong."+e);
-    	}
+			System.out.println("Something went wrong."+e);
+		}
     	System.out.println(j);
     	System.out.println("Bye");
     }
